8fe7e9a1f0a63dc60340628ed4a27127377d9bb0
lawsojf <lawsojf@mail.uc.edu>
2022-09-22 16:56:22 -0400

    Lab 3

---
+++ powershell/Mod3_Send_MailMessage.ps1
@@ -0,0 +1,19 @@
+﻿function getIP {
+(Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+
+function hostName {
+$HOST.Version.Major
+}
+
+function getDate {
+Get-Date -UFormat "%A, %B %d, %Y"
+}
+
+$getIP = getIP
+$hostName = hostName
+$getDate = getDate
+
+$BODY = "This machine's IP is $getIP. User is $env:USERNAME. Host-name is $env:COMPUTERNAME. Powershell Version $hostName. Today's date is $getDate."
+
+echo $BODY > mod3_output.txt
--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -6,4 +6,4 @@ write-host(getIP)
 $IP = getIP
 
 Write-Host("This machine's IP is $IP")
-Write-Host("This machine's IP is {0}" -f $IP")
+Write-Host("This machine's IP is {0}" -f $IP)
